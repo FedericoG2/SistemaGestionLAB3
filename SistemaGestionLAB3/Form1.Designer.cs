afc6c3c373a4f817dedd03ba9eb2371d5eee190b
FedericoG2 <fg0170700@gmail.com>
2024-09-17 21:09:30 -0300

    probando la rama

--- SistemaGestionLAB3/Form1.Designer.cs
+++ SistemaGestionLAB3/Form1.Designer.cs
@@ -44,9 +44,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(97, 130);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Login";
+            this.label1.Text = "Prueba";
             // 
             // Form1
             // 
