6b6fe1cb6e7ea1e90565dab11cd2e740da6750fa
Tomas Mercado <tomasfmercado@gmail.com>
2024-10-18 12:47:33 -0300

    Submos rama nueva

--- SistemaGestionLAB3/Controlador/DbLogin.cs
+++ SistemaGestionLAB3/Controlador/DbLogin.cs
@@ -26,7 +26,7 @@ namespace SistemaGestionLAB3.Controlador
                 using (OleDbConnection conexion = new OleDbConnection(ruta))
                 {
                     conexion.Open();
-                    //abrimos
+                    //abrimos de nuevo
                     string queryVerificar = "SELECT COUNT(*) FROM Usuarios WHERE Username = ? AND Contraseña = ?";
                     using (OleDbCommand comando = new OleDbCommand(queryVerificar, conexion))
                     {
