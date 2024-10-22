27a259c14e48c47dacac7954da02b9be70ac643d
Tomas Mercado <tomasfmercado@gmail.com>
2024-10-22 20:49:09 -0300

    Subiendo rama login en etapa final

--- SistemaGestionLAB3/Vista/FrmRegistrarse.cs
+++ SistemaGestionLAB3/Vista/FrmRegistrarse.cs
@@ -22,8 +22,28 @@ namespace SistemaGestionLAB3.Vista
         {
             InitializeComponent();
             this.StartPosition = FormStartPosition.CenterScreen;
+            // Asignar los eventos KeyDown a los TextBox
+            txtNombreUsuario.KeyDown += new KeyEventHandler(txtNombreUsuario_KeyDown);
+            txtContraseña.KeyDown += new KeyEventHandler(txtContraseña_KeyDown);
+        }
+        // metodo para manejar el evento del enter
+        private void txtNombreUsuario_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                // Llama al mismo método que se utiliza en btnIngresar_Click
+                btnRegistrarse.PerformClick();
+            }
         }
 
+        private void txtContraseña_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                // Llama al mismo método que se utiliza en btnIngresar_Click
+                btnRegistrarse.PerformClick();
+            }
+        }
         private void btnCancelar_Click(object sender, EventArgs e)
         {
             this.Close();
