788cd0bf2c54f100e7d22a81224b9616db6a008e
juliomadero21 <juliomadero21@gmail.com>
2024-11-01 22:44:24 -0300

    borrar espacios

--- SistemaGestionLAB3/Program.cs
+++ SistemaGestionLAB3/Program.cs
@@ -17,8 +17,6 @@ namespace SistemaGestionLAB3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-           
             Application.Run(new FrmLogin());
 
         }
